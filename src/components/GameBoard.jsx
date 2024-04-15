5eb7362674fdb55f61a9a0421d0459b7b8550d28
aamiryaqoob <aamiryaqoob965@gmail.com>
2024-04-16 01:38:13 +0500

    updated

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,6 +6,7 @@ const initialGameBoard = [
 
 export default function GameBoard() {
     return (
+        
         <ol id="game-board">
             {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
