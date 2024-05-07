246d209895540cdb73695556cd40b3f55ceb88e1
aamiryaqoob <aamiryaqoob965@gmail.com>
2024-05-07 23:49:57 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
+import Log from "./components/Log";
 
 
 function App() {
@@ -35,6 +36,7 @@ function App() {
         </ol>
         <GameBoard turns={gameTurns} onSelectSquare={handleSelectSquare} />
       </div>
+      <Log turns={gameTurns} />
     </main>
   )
 }
